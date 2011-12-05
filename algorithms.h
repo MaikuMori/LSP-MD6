dad0b1de5e21cfba04866a8ba3dd648eb970190b
Martins Balodis <martins256@gmail.com>
2011-12-05 22:17:04 +0200

    Added: best, worst fit function for testing

--- algorithms.h
+++ algorithms.h
@@ -5,3 +5,5 @@
 
 int alg_first_fit(MemoryBlock * memorylist_head, FILE * data);
 int alg_next_fit(MemoryBlock * memorylist_head, FILE * data);
+int alg_best_fit(MemoryBlock * memorylist_head, FILE * data);
+int alg_worst_fit(MemoryBlock * memorylist_head, FILE * data);
